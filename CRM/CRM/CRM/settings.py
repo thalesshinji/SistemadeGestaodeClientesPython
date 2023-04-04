b116d6a771d57b083be388a928ebe99a3d936b21
Thales <thalesshinji.technik@gmail.com>
2023-04-04 20:59:19 -0300

    Agregando funções na pagina de Lista do Customer

--- CRM/CRM/CRM/settings.py
+++ CRM/CRM/CRM/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "customer",
     "core",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- CRM/CRM/customer/views.py
+++ CRM/CRM/customer/views.py
@@ -1,10 +1,21 @@
+# Create your views here.
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import customer
+from .forms import CustomerForm
+from django.urls import reverse
 class CustomerListView(ListView) :
     template_name = "customer/customer_list.html"
     model = customer
     queryset = customer.objects.all()
 
-# Create your views here.
- 
+
+class CustomerCreateView(CreateView):
+    template_name = "customer/customer.html"
+    form_class = CustomerForm
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('customer:customer-list')
