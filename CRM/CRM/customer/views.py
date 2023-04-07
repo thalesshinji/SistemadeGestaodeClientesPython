fefefb7071a397ea68b3f863c946a2016d5f0a5d
Thales <thalesshinji.technik@gmail.com>
2023-04-06 21:58:06 -0300

    Adicionando as funções de update e delete.

--- CRM/CRM/customer/views.py
+++ CRM/CRM/customer/views.py
@@ -1,9 +1,10 @@
 # Create your views here.
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import customer
 from .forms import CustomerForm
 from django.urls import reverse
+from django.shortcuts import get_object_or_404
 class CustomerListView(ListView) :
     template_name = "customer/customer_list.html"
     model = customer
@@ -16,6 +17,29 @@ class CustomerCreateView(CreateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('customer:customer-list')
+    
+class CustomerUpdateView(UpdateView):
+    template_name = "customer/customer.html"
+    form_class = CustomerForm
+
+    def get_object(self):
+        id = self.kwargs.get("id")
+        return get_object_or_404(customer, id=id)
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('customer:customer-list')
+
+class CustomerDeleteView(DeleteView):
+
+    def get_object(self):
+        id = self.kwargs.get("id")
+        return get_object_or_404(customer, id=id)
     
     def get_success_url(self):
         return reverse('customer:customer-list')
